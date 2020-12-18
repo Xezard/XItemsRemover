fcc0cadf0337f9504e8832f248f543bc4cd6a0ee
Xezard <sanya.zikranets@yandex.ru>
2020-12-18 17:23:47 +0400

    Added util class to translate material name into readable string.

---
+++ src/main/java/ru/xezard/items/remover/utils/Materials.java
@@ -0,0 +1,33 @@
+/*
+ *  This file is part of XItemsRemover,
+ *  licensed under the Apache License, Version 2.0.
+ *
+ *  Copyright (c) Xezard (Zotov Ivan)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package ru.xezard.items.remover.utils;
+
+import lombok.experimental.UtilityClass;
+import org.bukkit.Material;
+
+@UtilityClass
+public class Materials
+{
+    public String toString(Material material)
+    {
+        String materialName = material.name().replace("_", " ");
+
+        return materialName.substring(0, 1).toUpperCase() + materialName.substring(1).toLowerCase();
+    }
+}
