22cd00601f9a4c295c64feea125e7c527ce931e0
Xezard <sanya.zikranets@yandex.ru>
2021-12-25 14:25:30 +0300

    Create ProjectileHitListener.java

---
+++ src/main/java/ru/xezard/items/remover/listeners/ProjectileHitListener.java
@@ -0,0 +1,45 @@
+/*
+ *  This file is part of XItemsRemover,
+ *  licensed under the Apache License, Version 2.0.
+ *
+ *  Copyright (c) Xezard (Zotov Ivan)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package ru.xezard.items.remover.listeners;
+
+import lombok.AllArgsConstructor;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import ru.xezard.items.remover.data.ItemsManager;
+
+@AllArgsConstructor
+public class ProjectileHitListener
+implements Listener
+{
+    private final ItemsManager itemsManager;
+
+    @EventHandler
+    public void onHit(ProjectileHitEvent event)
+    {
+        if (event.getEntityType() != EntityType.ARROW)
+        {
+            return;
+        }
+
+        // this.itemsManager.addEntity(event.getEntity());
+    }
+}
