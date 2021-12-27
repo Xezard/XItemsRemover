c8049e28336ed3d92923c614000a2fb8f4ba8033
Xezard <sanya.zikranets@yandex.ru>
2021-12-27 08:51:59 +0300

    Update TrackData.java

--- src/main/java/ru/xezard/items/remover/data/TrackData.java
+++ src/main/java/ru/xezard/items/remover/data/TrackData.java
@@ -26,7 +26,7 @@ import java.util.TreeMap;
 @Value
 public class TrackData
 {
-    NavigableMap<Long, String> displayNames = new TreeMap<> ();
+    NavigableMap<Long, String> displayNames;
 
     long timer;
 
